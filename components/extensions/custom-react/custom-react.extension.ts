a923d905d897270e4608aecaf79f35ef14f575b4
rob <rob@pocketpoints.com>
2021-04-23 14:50:04 -0700

    workaround

--- components/extensions/custom-react/custom-react.extension.ts
+++ components/extensions/custom-react/custom-react.extension.ts
@@ -2,6 +2,7 @@ import { EnvsMain, EnvsAspect } from '@teambit/envs'
 import { ReactAspect, ReactMain } from '@teambit/react'
 
 const webpackConfig = require('./webpack.config');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 export class CustomReactExtension {
     constructor(private react: ReactMain) { }
@@ -11,7 +12,17 @@ export class CustomReactExtension {
     static async provider([envs, react]: [EnvsMain, ReactMain]) {
         const customReactEnv = react.compose([
             react.overrideDevServerConfig(webpackConfig),
-            react.overridePreviewConfig(webpackConfig),
+            react.overridePreviewConfig({
+                entry: ['./components/styles/tailwind/tailwind.css'],
+                plugins: [
+                    new WebpackShellPlugin({
+                        safe: true,
+                        onBuildStart: [
+                            'cd ./components/styles/tailwind && npm i && npm run build'
+                        ]
+                    })
+                ]
+            })
         ])
 
         envs.registerEnv(customReactEnv)
